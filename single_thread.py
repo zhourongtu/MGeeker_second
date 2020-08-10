e44cb5ecdfcb122c742158e266c4328c019ad140
zhourongtu <zhourongtu@qq.com>
2020-08-10 14:55:14 +0800

    解决了部分问题

--- single_thread.py
+++ single_thread.py
@@ -45,6 +45,7 @@ def query_nearest(x_loc_yewu, y_loc_yewu):
             continue #下一个范围继续查找
 
     # 解决问题2，进行根号二倍放大
+    result = None
     if(flag): 
         x_limit_l, x_limit_r, y_limit_l, y_limit_r = expand_range_times_sqrt2([x_limit_l, x_limit_r, y_limit_l, y_limit_r])
         sql = 'select id, x_loc, y_loc from user_info where x_block >= {0} and x_block <= {1} and y_block >= {2} and y_block <= {3}'.format(x_limit_l, x_limit_r, y_limit_l, y_limit_r)
@@ -53,18 +54,18 @@ def query_nearest(x_loc_yewu, y_loc_yewu):
         my_dict = {}
         for data in datas: # 进行处理与排序，如果距离相同，比较用户id大小，做替换
             distance = round((float(data[1])-x_loc_yewu)**2 + (float(data[2])-y_loc_yewu)**2, 4)
-            if(distance in my_dict):
+            if(my_dict.get(distance)):
                 if(my_dict[distance][0] > data[0]): #用户编号更小
                     my_dict[distance] = data
             else:
                 my_dict[distance] = data
-        result = sorted(my_dict.items(),key=lambda my_dict:my_dict[0],reverse=False)
+        result = my_dict[min(my_dict.keys())] # 距离最小
     # 关闭数据库连接
     cursor.close()
     conn.close()
     # 距离公式，a^2 + b^2，排序
     if(flag):
-        return result[0][1] # 结果
+        return result # 结果
     else:
         return None
 

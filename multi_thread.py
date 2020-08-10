d779b7e2f08eab9d02ccda80c5e45fa638256acf
zhourongtu <zhourongtu@qq.com>
2020-08-10 15:16:12 +0800

    多线程处理

--- multi_thread.py
+++ multi_thread.py
@@ -30,11 +30,17 @@ def multi_yewu(list_of_locs):
         t = thread_process_yewu(single_thread.query_nearest, args=(x_loc_yewu, y_loc_yewu))
         multi_to_process.append(t)
         t.start()
-        print(x_loc_yewu, y_loc_yewu)
     
+    ans = []
     for t in multi_to_process:
         t.join()
-        print(t.get_result(), t.getName())
+        ans.append([t.get_result()[0], t.get_result()[1], t.get_result()[2], t.getName()])
+    
+    for user, my_loc in zip(ans, list_of_locs):
+        x_loc_yewu = round(float(my_loc[0]), 2)
+        y_loc_yewu = round(float(my_loc[1]), 2)
+        print('业务员坐标：', x_loc_yewu, y_loc_yewu)
+        print('用户编号：', user[0], '用户坐标：', user[1], user[2], '处理线程名：', user[3])
 
 list_of_locs = [[1.512, 2.213], [32,56], [102.54, 888.66], [825.5435, 666.66],[1.512, 2.213], [32,56], [102.54, 888.66], [825.5435, 666.66]]
 

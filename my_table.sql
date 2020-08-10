54ec1b1b6fc1e89569e7fffdfc282470a810f1e2
zhourongtu <zhourongtu@qq.com>
2020-08-10 22:06:10 +0800

    修改并添加了建表的语句

--- my_table.sql
+++ my_table.sql
@@ -4,5 +4,13 @@ create table user_info(
     y_loc float,
     x_block int,
     y_block int,
+    primary key(id),
     index(x_block, y_block)
+);
+
+create table user_info_test_1(
+	id int not null,
+    x_loc float,
+    y_loc float,
+    primary key(id)
 );
